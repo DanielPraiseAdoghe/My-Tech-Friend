dcf4a9e64d2fdcd92496b9888570c7f67e372e7d
Praise Oghosa Adoghe <danieladoghe01@gmail.com>
2026-02-21 05:14:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ login_button.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = 'Dashboard/home.html';
 });
+
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
